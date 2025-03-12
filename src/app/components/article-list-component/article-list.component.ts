92fe18fa0eb709c5a253a0d726e1ee2a9edcec58
alesbesdria <melissa@meirsman.fr>
2025-03-12 09:39:09 +0100

    save

--- src/app/components/article-list-component/article-list.component.ts
+++ src/app/components/article-list-component/article-list.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Article } from '../../models/article.model';
 
 @Component({
-  selector: 'app-article-list-component',
-  imports: [FormsModule, Article],
+  selector: 'app-article-list',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './article-list.component.html',
-  styleUrl: './article-list.component.scss'
+  styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent {
   title = "Articles Populaires";
-  article = [Article];
+  articles: Article[] = [
+    { 
+      id: 1,
+      title: 'Angular 16: Les nouveautés', 
+      content: 'Les nouveautés d\'Angular 16 incluent...', 
+      createdAt: new Date('2023-05-01'),
+      image: 'https://placehold.co/150x150',
+      likeCount: 233,
+      isPublished: true,
+      categoryName: 'Angular',
+      isLiked: false
+    },
+    { 
+      id: 2,
+      title: 'Développer une API REST', 
+      content: 'Développer une API REST nécessite de bonnes pratiques...', 
+      createdAt: new Date('2023-04-15'),
+      image: 'https://placehold.co/150x150',
+      likeCount: 99,
+      isPublished: true,
+      categoryName: 'API',
+      isLiked: false
+    }
+  ];
 }
--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,22 +1,11 @@
-import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
-
-@Component({
-  selector: 'app-article',
-  imports: [DatePipe],
-  templateUrl: './article.model.html',
-  styleUrl: './article.model.scss'
-})
-export class Article {
-  title = 'Titre de l\'article';
-  content = 'Voici le contenu de l\'article.';
-  image = 'https://placehold.co/150x150';
-  createdAt = new Date();
-  isPublished = false;
-  likeCount = 0;
-  categoryName = 'Angular';
-
-  likeButton(): void {
-    this.likeCount += 1;
-  }
+export interface Article {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+  createdAt: Date;
+  isPublished: boolean;
+  isLiked: boolean;
+  likeCount: number;
+  categoryName: string;
 }

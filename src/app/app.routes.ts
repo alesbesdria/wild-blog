9d8d3aa4c4fa07205cc90d05079d0ef63845fc00
alesbesdria <melissa@meirsman.fr>
2025-03-14 18:47:36 +0100

    contact angular quest 6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArticlePageComponent } from './components/article-page/article-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { ArticleListComponent } from './components/article-list-component/article-list.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent, pathMatch: 'full' },
-    // { path: 'contact', component: ContactPageComponent },
+    { path: 'contact', component: ContactPageComponent },
     { path: 'articles/:id', component: ArticlePageComponent },
     { path: 'articles', component: ArticleListComponent },
 

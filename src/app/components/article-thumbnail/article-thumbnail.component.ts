53a3e7b268eea6e4f1b519f4eed27792579aeb90
alesbesdria <melissa@meirsman.fr>
2025-03-15 13:24:42 +0100

    child parent angular quest 8

--- src/app/components/article-thumbnail/article-thumbnail.component.ts
+++ src/app/components/article-thumbnail/article-thumbnail.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
-import { ArticleListComponent } from '../article-list-component/article-list.component';
+import { Component, Input } from '@angular/core';
+import { Article } from '../../models/article.model';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-thumbnail',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './article-thumbnail.component.html',
   styleUrl: './article-thumbnail.component.scss'
 })
 export class ArticleThumbnailComponent {
+  @Input() article!: Article;
   /////////////////// MESSAGE PARENT ENFANT
-  @Input() parentMessage: string = '';
+  // @Input() parentMessage: string = '';
   /////////////////// 
 }
